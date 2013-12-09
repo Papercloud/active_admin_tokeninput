7ef6286a57078e15a59af2f91903285bedb6cd75
William Porter <dawilster143@gmail.com>
2013-12-09 17:06:41 +1100

    infer collection param

--- lib/active_admin_taginput/dsl.rb
+++ lib/active_admin_taginput/dsl.rb
@@ -2,7 +2,7 @@ module ActiveAdminTaginput
   module DSL
   	def tag_autocomplete(options={})
   		collection_action :autocomplete, :method => :get do
-  			collection = options[:collection].where("name ILIKE ?", "%#{params[:q]}%").order('lower(name) ASC').limit(10)
+  			collection = active_admin_config.resource_class.where("name ILIKE ?", "%#{params[:q]}%").order('lower(name) ASC').limit(10)
   		 	render :json => collection.as_json(:only => [:id, :name]), root: false
   		end
 	end
