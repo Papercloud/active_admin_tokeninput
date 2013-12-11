267398c51e3d35ea07279a831072e0bc793ae528
William Porter <dawilster143@gmail.com>
2013-12-11 11:56:43 +1100

    scoped api results

--- lib/active_admin_tokeninput/dsl.rb
+++ lib/active_admin_tokeninput/dsl.rb
@@ -1,8 +1,10 @@
 module ActiveAdminTokeninput
   module DSL
   	def tag_autocomplete(options={})
+
+
   		collection_action :autocomplete, :method => :get do
-  			collection = active_admin_config.resource_class.where("name ILIKE ?", "%#{params[:q]}%").order('lower(name) ASC').limit(10)
+  			collection = scoped_collection.where("name ILIKE ?", "%#{params[:q]}%").order('lower(name) ASC').limit(10)
   		 	render :json => collection.as_json(:only => [:id, :name]), root: false
   		end
 	end
