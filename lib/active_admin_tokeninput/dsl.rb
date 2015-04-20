b555224b35a105e16776d8ce8a5c3f73b1f522da
Isaac Norman <isaacdnorman@gmail.com>
2015-04-20 13:56:30 +1000

    Cast to text. For situations where you might be searching by an integer (such as id)

--- lib/active_admin_tokeninput/dsl.rb
+++ lib/active_admin_tokeninput/dsl.rb
@@ -6,8 +6,8 @@ module ActiveAdminTokeninput
       collection_action :autocomplete, :method => :get do
         attribute = options[:attribute] || "name"
 
-        collection = scoped_collection.where("#{attribute} ILIKE ?", "%#{params[:q]}%")
-        collection = collection.order("lower(#{attribute}) ASC").limit(10) if attribute.is_a? String
+        collection = scoped_collection.where("CAST(#{attribute} as text) ILIKE ?", "%#{params[:q]}%")
+        collection = collection.order("lower(CAST #{attribute} as text) ASC").limit(10)
         render :json => collection.as_json(:only => [:id, attribute.to_sym]), root: false
       end
   end
