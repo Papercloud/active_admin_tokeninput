8d3d78f7a79f461146726f5c612cc43399a3c194
Isaac Norman <isaacdnorman@gmail.com>
2015-05-12 11:46:24 +1000

    Remove the try, because we are converting to string now

--- app/inputs/belongs_to_token_input.rb
+++ app/inputs/belongs_to_token_input.rb
@@ -4,7 +4,7 @@ class BelongsToTokenInput < Formtastic::Inputs::StringInput
 		super.merge(
 			"data-token-limit" => 1,
 			"class" => "active-admin-tokeninput",
-			"data-field-name" => "#{object_name.to_s.try(:underscore)}[#{method.to_s.singularize}_id]",
+			"data-field-name" => "#{object_name.to_s.underscore}[#{method.to_s.singularize}_id]",
 			"data-pre" => "[#{object.send("#{method}").to_json(:only => [:id, :name])}]",
 			"name" => "",
 			"value" => "",
