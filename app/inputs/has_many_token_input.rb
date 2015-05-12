af814aefefb9198f77f114432b7f1efb0ed71853
Isaac Norman <isaacdnorman@gmail.com>
2015-05-12 11:42:27 +1000

    Much cleaner solution, allow unscoped as an option rather than a whole new input

--- app/inputs/has_many_token_input.rb
+++ app/inputs/has_many_token_input.rb
@@ -5,7 +5,7 @@ class HasManyTokenInput < Formtastic::Inputs::StringInput
 		super.merge(
 			"class" => "active-admin-tokeninput",
 			"data-field-name" => "#{object_name.underscore}[#{method.to_s.singularize}_ids][]",
-			"data-pre" => object.send("#{method}").map.to_json(:only => [:id, search_property.to_sym]),
+			"data-pre" => options[:unscoped] == true ? nil : object.send("#{method}").map.to_json(:only => [:id, search_property.to_sym]),
 			"name" => "",
 			"value" => "",
 			"data-autocomplete-path" => options[:autocomplete_path] ||= Rails.application.routes.url_helpers.send("autocomplete_admin_#{method.to_s}_path"),
